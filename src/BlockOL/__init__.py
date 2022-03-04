b3213c7971468079b58f88a782a3c8ed5cc37d9d
BlockOfGaming <alesiobaca@gmail.com>
2022-03-04 16:33:34 +0200

    Fixed and added some stuff, should be stable now

--- src/BlockOL/__init__.py
+++ src/BlockOL/__init__.py
@@ -15,7 +15,7 @@ def prev_prime(n):
     n -= 1
     while not is_prime(n):
         n -= 1
-        if n < 0: return None
+        if n < 2: return None
     return n
 
 def next_prime(n):
@@ -24,13 +24,19 @@ def next_prime(n):
         n += 1
     return n
 
-def fibonacci(end):
+def fibn(endn):
     fibs = [1, 1]
-    while fibs[-1] < end:
+    while fibs[-1] < endn:
+        fibs.append(fibs[-2] + fibs[-1])
+    return fibs[:-1]
+
+def fibi(endi):
+    fibs = [1, 1]
+    for _ in range(endi - 2):
         fibs.append(fibs[-2] + fibs[-1])
     return fibs
 
-def zeros_afer(n):
+def zeros_after(n):
     a = 0
     
     for i in str(n)[::-1]:
@@ -42,18 +48,24 @@ def zeros_afer(n):
 
 def counter(dic, ns):
     for n in ns:
-        if n in dic:
-            dic[n] += 1
-        else:
-            dic[n] = 1
+        dic[n] = dic.get(n, 0) + 1
     
     return dic
 
-def diviz(n, start=2):
-    if is_prime(n):
+def diviz(n):
+    if is_prime(n): return [1, n]
+    if n in (0, 1): return [1] * n
+    a = [1]
+    
+    for i in range(2, n - 1):
+        if n % i == 0:
+            a.append(i)
+    
+    return a + [n]
+
+def diviz_fl(n, start=2):
+    if is_prime(n) or n in (0, 1):
         return [1, n]
-    if n in (0, 1):
-        return [1] * n
     
     start = abs(start)
     
@@ -61,23 +73,29 @@ def diviz(n, start=2):
         if n % i == 0:
             return [i, int(n / i)]
 
+def largest_prime_div(n):
+    i = 2
+    
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    
+    return n
+
 def prime_div(n):
-    a = {}
-    b = 2
-    _f = zeros_afer(n)
+    i = 2
+    factors = []
     
-    if _f > 0 and n != 0:
-        n = int(n / pow(10, _f))
-        a = {2: _f, 5: _f}
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
     
-    e = diviz(n, b)
-    while e[0] > 1:
-        n = e[1]
-        a = counter(a, [e[0]])
-        b = e[0]
-        e = diviz(n, b)
+    if n > 1:
+        factors.append(n)
     
-    if n and n - 1:
-        a = counter(a, [n])
-    
-    return a
+    return factors
